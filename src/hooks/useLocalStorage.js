5694be1759dd5b9ffc30d48ea6cb7663496aaeb7
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-09-04 16:25:52 -0700

    MVP

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,18 +3,14 @@ import { useState } from 'react';
 function useLocalStorage (key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
-        // if (item) {
-        //     return JSON.parse(item);
-        // } else {
-        //     return initialValue;
-        // }
+        
         return (item ? JSON.parse(item) : initialValue);
     });
     const setValue = value => {
         setStoredValue(value);
         window.localStorage.setItem(key, JSON.stringify(value));
     };
-    return [storedValue, setValue, ]
+    return [storedValue, setValue]
 }
 
 export default useLocalStorage;
