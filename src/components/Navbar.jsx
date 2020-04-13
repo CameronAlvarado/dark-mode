a5b8c3e82520e36aae666370730ef3a01edf4e45
CameronAlvarado <cameronapple777@gmail.com>
2020-04-13 16:32:21 -0700

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import useDarkMode from '../hooks/useDarkMode.js';
+import React, { useState } from "react";
+import useDarkMode from "../hooks/useDarkMode.js";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
-  const toggleMode = e => {
+  const toggleMode = (e) => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <div onClick={toggleMode}
-      className="dark-mode__toggle">
-        <div
-          className={darkMode ? 'toggle toggled' : 'toggle'}
-        />
+      <div onClick={toggleMode} className="dark-mode__toggle">
+        <div className={darkMode ? "toggle toggled" : "toggle"} />
       </div>
     </nav>
   );
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,11 +4,7 @@ import useLocalStorage from './useLocalStorage.js';
 function useDarkMode(initialValue) {
     const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initialValue);
     useEffect(() => {
-        // if (darkMode) {
-        //     document.body.classList.add('dark-mode');
-        // } else {
-        //     document.body.classList.remove('dark-mode');
-        // }
+
         return (darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode'));
     }, [darkMode]);
     return [darkMode, setDarkMode];
